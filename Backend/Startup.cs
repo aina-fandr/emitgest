df3ea9cee6ffe2c5e62f78d0bb4d334c5f90817e
Ny Aina Fandresena <ainafandresena2006@gmail.com.com>
2026-05-16 11:30:29 -0700

    description des modifications

---
+++ Backend/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+
+namespace MonProjetWeb
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // Servir les fichiers du dossier Frontend
+            app.UseDefaultFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), "..", "Frontend")),
+                RequestPath = ""
+            });
+        }
+    }
+}
