a263430652dc9c485078fc20e6be070eb7667768
Ny aina fandresena <ainafandresena2006@gmail.com>
2026-05-17 22:48:53 -0700

    app.jsx maj

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Home from './pages/Home'
 import Loginsco from './pages/scolarite/Loginsco'
 import ScolariteApp from './pages/scolarite/ScolariteApp'
+import Loginprof from './pages/professeur/Loginprof'
 import ProfesseurApp from './pages/professeur/ProfesseurApp'
 import PublicApp from './pages/public/PublicApp'
 
@@ -13,19 +14,22 @@ function App() {
   switch (currentView) {
     case 'home':
       return <Home onNavigate={(page) => setCurrentView(page)} />
-    
+
     case 'loginsco':
       return <Loginsco onBack={goHome} onLogin={() => setCurrentView('scolarite')} />
-    
+
     case 'scolarite':
       return <ScolariteApp onBack={goHome} />
-    
-    case 'professeur':
+
+    case 'loginprof':
+      return <Loginprof onBack={goHome} onLogin={() => setCurrentView('professeurapp')} />
+
+    case 'professeurapp':
       return <ProfesseurApp onBack={goHome} />
-    
+
     case 'public':
       return <PublicApp onBack={goHome} />
-    
+
     default:
       return <Home onNavigate={(page) => setCurrentView(page)} />
   }
